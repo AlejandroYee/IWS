fc05ce57bb46b9bd65c9ecd85cd9577525e5e16a
andrey-boomer <andrey.boomer@gmail.com>
2013-07-25 13:40:16 +0400

    Исправлен символ в логе

--- library/db.oci.php
+++ library/db.oci.php
@@ -176,7 +176,7 @@ var $link, $user_pref, $user_real_name,$session_id_local;
 		
 		// Если был ретурнинг то отдаем вместо ресурса вернувшиеся значение
 		if (!empty($rowid)) {
-					to_log("SQL: <".$this->link."> returning id ".$rowid." ha reached!");
+					to_log("SQL: <".$this->link."> returning id ".$rowid." is a reached!");
 					return $rowid;
 			} else {		
 					// Возвращаем ресурс для извлечения данных
