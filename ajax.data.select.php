056e2fa90f0f650e894d62dbea9c18abe2dfab69
andrey-boomer <andrey.boomer@gmail.com>
2014-01-15 13:40:33 +0400

    Убираем лишние цифры из имени

--- ajax.data.select.php
+++ ajax.data.select.php
@@ -13,14 +13,14 @@
 	header("Content-type: text/script;charset=".HTML_ENCODING);
 	
         $parent_id      = filter_input(INPUT_GET, 'parent_id',FILTER_SANITIZE_STRING);
-	$value_name     = trim_fieldname(filter_input(INPUT_GET, 'value_name',FILTER_SANITIZE_STRING));
+	$value_name     = filter_input(INPUT_GET, 'value_name',FILTER_SANITIZE_STRING);
 	$value_id       = substr($value_name, strrpos($value_name, "_") + 1);
 	$select         = "";
 	$result_array   = "";
         
 	$main_db = new db();
 	
-	$query = $main_db -> sql_execute("select fw.field_txt from ".DB_USER_NAME.".wb_form_field fw where lower(fw.field_name) = lower('".$value_name."') and abs(fw.id_wb_form_field) = '".$value_id."'");		
+	$query = $main_db -> sql_execute("select fw.field_txt from ".DB_USER_NAME.".wb_form_field fw where lower(fw.field_name) = lower('".BasicFunctions::trim_fieldname($value_name)."') and abs(fw.id_wb_form_field) = '".$value_id."'");		
         BasicFunctions::end_session(); 
 	while ($main_db -> sql_fetch($query)) {
 		$select = $main_db -> sql_result($query, "FIELD_TXT");
